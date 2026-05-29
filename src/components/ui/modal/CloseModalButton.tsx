0fd59e254b3f38424fd1d6e89c54fef5a525eda4
Javier Villca <javieranibalvillca@gmail.com>
2026-05-29 20:03:24 -0300

    fix styles

--- src/components/ui/modal/CloseModalButton.tsx
+++ src/components/ui/modal/CloseModalButton.tsx
@@ -8,8 +8,9 @@ const CloseModalButton = ({ onClose }: PropTypes) => {
   return (
     <button
       onClick={onClose}
-      className="absolute top-3 right-3 p-2 hover:bg-gray-200 rounded-full"
+      className="absolute top-3 right-3 p-2 hover:bg-gray-200 rounded-full cursor-pointer"
       data-test="close-modal-button"
+      type="button"
     >
       <IoMdClose className="text-[22px] lg:text-[32px]" />
     </button>
